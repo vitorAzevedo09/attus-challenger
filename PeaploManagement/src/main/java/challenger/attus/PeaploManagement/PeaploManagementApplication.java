87d3038204e06d0294effa36aa17f0dfc43b0d20
Vitor Silva <vitor.azevedo009@gmail.com>
2024-05-09 10:10:12 -0300

    Initial commit

---
+++ PeaploManagement/src/main/java/challenger/attus/PeaploManagement/PeaploManagementApplication.java
@@ -0,0 +1,13 @@
+package challenger.attus.PeaploManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaploManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaploManagementApplication.class, args);
+	}
+
+}
---
+++ PeaploManagement/src/test/java/challenger/attus/PeaploManagement/PeaploManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package challenger.attus.PeaploManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaploManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
